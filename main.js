f8bb874e961a62cb556987459d93d9032d9d9e7f
Goreth Manuel <gorethmanuel0@gmail.com>
2022-08-15 22:18:10 +0100

    Se aceita adiciona atualizacoes

--- main.js
+++ main.js
@@ -3,7 +3,60 @@ window.addEventListener('scroll', onscroll)
 onScroll()
 function onScroll() {
   showNavOnScroll()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+
+
+}
+
+function activateMenuAtCurrentSection(section) {
+  // linha alvo
+  const targetLine = scrollY + innerHeight / 2
+
   
+  // verificar se a seção passou da linha
+
+  //o topo da seção
+  const sectionTop = section.offsetTop
+
+  //a altura da seção
+  const sectionHeight = section.offsetHeight
+
+  // o topo da seção chegou ou ultrapassou a linha alto
+
+  const sectionTopReachOrPassedTargetline = targetLine 
+  >= sectionTop
+
+
+   // a seção termina onde?
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  // o final da seção passou da linha alvo  
+  const sectionEndPassedTargetline = sectionEndsAt <= 
+  targetLine
+
+
+  //limites da seção
+  const sectionBoundaries = 
+  sectionTopReachOrPassedTargetline && 
+  !sectionEndPassedTargetline
+  
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document
+  .querySelector(`.menu a[href*=${sectionId}]`)
+  
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+
+  }
+
+ 
+
 }
 //showBackToTopButtonOnScroll()
 
